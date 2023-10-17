59156e71fe9562576b2950785c6fb2c88dbcdf1a
pjnsajj <nazirbekovjusup@gmai.com>
2023-10-17 15:50:22 +0600

    first commit

---
+++ cardgame/lib/resources/app_colors.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+ class AppColors {
+  static const blue = Color(0xFF007AFF);
+  static const darkBlue = Color(0xFF0061D5);
+  static const accentBlue = Color(0xFF31A9FE);
+  static const accentBlue2 = Color(0xFF15DAF5);
+  static const yellow = Color(0xFFFFFF00);
+  static const yellow2 = Color.fromARGB(255, 228, 177, 47);
+  static const yellow3 = Color.fromARGB(255, 221, 137, 47);
+  static const background = Color(0xFF0B1D2E);
+  static const accentPurple = Color(0xFFF95DEA);
+  static const accentRed = Color(0xFFFF4D4D);
+  static const accentRed2 = Color(0xFFF54F52);
+  static const red = Color(0xFFE23B21);
+  static const red2 = Color(0xFFFF0000);
+  static const pink = Color(0xFFF6496B);
+  static const pink2 = Color(0xFFFF97BC);
+}
